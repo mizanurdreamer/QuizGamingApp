f00ffeade23e82032c7e485cff08a2e43dcab2af
Zubayed <zubayed2008@yahoo.com>
2020-02-06 16:39:21 +0600

    fix some issue

--- QuizGamingApp.Core/DAL/Initial/QuizGamingAppDBRepository.cs
+++ QuizGamingApp.Core/DAL/Initial/QuizGamingAppDBRepository.cs
@@ -12,6 +12,8 @@ namespace QuizGamingApp.Core.DAL
 {
     public class QuizGamingAppDBRepository
     {
+        
+
         private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
         private static DocumentClient QuizGamingAppClient;
         private static void Initialize()
@@ -40,8 +42,11 @@ namespace QuizGamingApp.Core.DAL
         {
             try
             {
-                await QuizGamingAppClient.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseId));
-                
+                bool IsCreateDatabase = ConfigurationManager.AppSettings["IsCreateDatabse"] != null? bool.Parse(ConfigurationManager.AppSettings["IsCreateDatabse"]):false;
+                if (IsCreateDatabase)
+                {
+                    await QuizGamingAppClient.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseId));
+                }
             }
             catch (DocumentClientException e)
             {
@@ -58,150 +63,171 @@ namespace QuizGamingApp.Core.DAL
 
         private static async Task CreateTablesIfNotExists()
         {
-            new ClientProfileRepository().Initialize();
-            new PlayerProfileRepository().Initialize();
-            new QuizGameRepository().Initialize();
-            new QuizQuestionRepository().Initialize();
-            new QuestionAnswerRepository().Initialize();
-            new QuizGameAnswerRepository().Initialize();
+            try
+            {
+                bool IsCreateTable = ConfigurationManager.AppSettings["IsCreateTable"] != null ? bool.Parse(ConfigurationManager.AppSettings["IsCreateTable"]) : false;
+                if (!IsCreateTable)
+                {
+                    return;
+                }
+                new ClientProfileRepository().Initialize();
+                new PlayerProfileRepository().Initialize();
+                new QuizGameRepository().Initialize();
+                new QuizQuestionRepository().Initialize();
+                new QuestionAnswerRepository().Initialize();
+                new QuizGameAnswerRepository().Initialize();
 
-            new GameRepository().Initialize();
-            new GameCouponRepository().Initialize();
-            new GameModeRepository().Initialize();
-            new GameSubscriptionRepository().Initialize();
-            new GameSubscriptionTypeRepository().Initialize();
-            new GameTypeRepository().Initialize();
-            new PlayerLoginTypeRepository().Initialize();
-            new RewardDistributionTypeRepository().Initialize();
-            new RewardingPlayerTypeRepository().Initialize();
+                new GameRepository().Initialize();
+                new GameCouponRepository().Initialize();
+                new GameModeRepository().Initialize();
+                new GameSubscriptionRepository().Initialize();
+                new GameSubscriptionTypeRepository().Initialize();
+                new GameTypeRepository().Initialize();
+                new PlayerLoginTypeRepository().Initialize();
+                new RewardDistributionTypeRepository().Initialize();
+                new RewardingPlayerTypeRepository().Initialize();
+            }
+            catch (Exception)
+            {
+
+            }
+            
         }
 
         private static async Task InsertMasterDataAsync()
         {
-            GameTypeRepository gameTypeRepository = new GameTypeRepository();
-            GameModeRepository gameModeRepository = new GameModeRepository();
-            PlayerLoginTypeRepository playerLoginTypeRepository = new PlayerLoginTypeRepository();
-            RewardDistributionTypeRepository rewardDistributionTypeRepository = new RewardDistributionTypeRepository();
-            GameSubscriptionTypeRepository gameSubscriptionTypeRepository = new GameSubscriptionTypeRepository();
-            RewardingPlayerTypeRepository rewardingPlayerTypeRepository = new RewardingPlayerTypeRepository();
 
-            var gameTypeList = await gameTypeRepository.GetItemsAsync(d => d.GameTypeId == 1);
-            if(gameTypeList != null && gameTypeList.Any())
+            try
             {
-                return;
+                bool IsInsertMasterData = ConfigurationManager.AppSettings["IsInsertMasterData"] != null ? bool.Parse(ConfigurationManager.AppSettings["IsInsertMasterData"]) : false;
+                if (!IsInsertMasterData)
+                {
+                    return;
+                }
+                GameTypeRepository gameTypeRepository = new GameTypeRepository();
+                GameModeRepository gameModeRepository = new GameModeRepository();
+                PlayerLoginTypeRepository playerLoginTypeRepository = new PlayerLoginTypeRepository();
+                RewardDistributionTypeRepository rewardDistributionTypeRepository = new RewardDistributionTypeRepository();
+                GameSubscriptionTypeRepository gameSubscriptionTypeRepository = new GameSubscriptionTypeRepository();
+                RewardingPlayerTypeRepository rewardingPlayerTypeRepository = new RewardingPlayerTypeRepository();
+
+                GameType gameType = new GameType()
+                {
+                    GameTypeId = 1,
+                    GameTypeName = "Trivia",
+                };
+                await gameTypeRepository.CreateItemAsync(gameType);
+
+                PlayerLoginType playerLoginType = new PlayerLoginType()
+                {
+                    PlayerLoginTypeId = 1,
+                    PlayerLoginTypeName = "Anonymous"
+                };
+                await playerLoginTypeRepository.CreateItemAsync(playerLoginType);
+
+                playerLoginType = new PlayerLoginType()
+                {
+                    PlayerLoginTypeId = 2,
+                    PlayerLoginTypeName = "Registration"
+                };
+                await playerLoginTypeRepository.CreateItemAsync(playerLoginType);
+
+                RewardDistributionType rewardDistributionType = new RewardDistributionType()
+                {
+                    RewardDistributionTypeId = 1,
+                    RewardDistributionTypeName = "Winners"
+                };
+                await rewardDistributionTypeRepository.CreateItemAsync(rewardDistributionType);
+                rewardDistributionType = new RewardDistributionType()
+                {
+                    RewardDistributionTypeId = 2,
+                    RewardDistributionTypeName = "Losers"
+                };
+                await rewardDistributionTypeRepository.CreateItemAsync(rewardDistributionType);
+
+                rewardDistributionType = new RewardDistributionType()
+                {
+                    RewardDistributionTypeId = 3,
+                    RewardDistributionTypeName = "Everyone"
+                };
+                await rewardDistributionTypeRepository.CreateItemAsync(rewardDistributionType);
+
+                RewardingPlayerType rewardingPlayerType = new RewardingPlayerType()
+                {
+                    RewardingPlayerTypeId = 1,
+                    RewardingPlayerTypeName = "Lottery"
+                };
+                await rewardingPlayerTypeRepository.CreateItemAsync(rewardingPlayerType);
+
+                rewardingPlayerType = new RewardingPlayerType()
+                {
+                    RewardingPlayerTypeId = 2,
+                    RewardingPlayerTypeName = "Leaderboard"
+                };
+                await rewardingPlayerTypeRepository.CreateItemAsync(rewardingPlayerType);
+
+                rewardingPlayerType = new RewardingPlayerType()
+                {
+                    RewardingPlayerTypeId = 3,
+                    RewardingPlayerTypeName = "For all"
+                };
+                await rewardingPlayerTypeRepository.CreateItemAsync(rewardingPlayerType);
+
+                GameSubscriptionType gameSubscriptionType = new GameSubscriptionType()
+                {
+                    GameSubscriptionTypeId = 1,
+                    GameSubscriptionTypeName = "Daily"
+                };
+
+                await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
+
+                gameSubscriptionType = new GameSubscriptionType()
+                {
+                    GameSubscriptionTypeId = 2,
+                    GameSubscriptionTypeName = "1 Month"
+                };
+                await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
+
+                gameSubscriptionType = new GameSubscriptionType()
+                {
+                    GameSubscriptionTypeId = 3,
+                    GameSubscriptionTypeName = "3 Month"
+                };
+                await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
+
+                gameSubscriptionType = new GameSubscriptionType()
+                {
+                    GameSubscriptionTypeId = 4,
+                    GameSubscriptionTypeName = "1 Year"
+                };
+                await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
+
+                GameMode gameMode = new GameMode()
+                {
+                    GameModeId = 1,
+                    GameModeName = "Timer countdown"
+                };
+                await gameModeRepository.CreateItemAsync(gameMode);
+
+                gameMode = new GameMode()
+                {
+                    GameModeId = 2,
+                    GameModeName = "Points countdown"
+                };
+                await gameModeRepository.CreateItemAsync(gameMode);
+
+                gameMode = new GameMode()
+                {
+                    GameModeId = 3,
+                    GameModeName = "Round based"
+                };
+                await gameModeRepository.CreateItemAsync(gameMode);
             }
-
-            GameType gameType = new GameType()
+            catch(Exception)
             {
-                GameTypeId = 1,
-                GameTypeName = "Trivia",
-            };
-            await gameTypeRepository.CreateItemAsync(gameType);
 
-            PlayerLoginType playerLoginType = new PlayerLoginType()
-            {
-                PlayerLoginTypeId = 1,
-                PlayerLoginTypeName = "Anonymous"
-            };
-            await playerLoginTypeRepository.CreateItemAsync(playerLoginType);
-
-            playerLoginType = new PlayerLoginType()
-            {
-                PlayerLoginTypeId = 2,
-                PlayerLoginTypeName = "Registration"
-            };
-            await playerLoginTypeRepository.CreateItemAsync(playerLoginType);
-
-            RewardDistributionType rewardDistributionType = new RewardDistributionType()
-            {
-                RewardDistributionTypeId = 1,
-                RewardDistributionTypeName = "Winners"
-            };
-            await rewardDistributionTypeRepository.CreateItemAsync(rewardDistributionType);
-            rewardDistributionType = new RewardDistributionType()
-            {
-                RewardDistributionTypeId = 2,
-                RewardDistributionTypeName = "Losers"
-            };
-            await rewardDistributionTypeRepository.CreateItemAsync(rewardDistributionType);
-
-            rewardDistributionType = new RewardDistributionType()
-            {
-                RewardDistributionTypeId = 3,
-                RewardDistributionTypeName = "Everyone"
-            };
-            await rewardDistributionTypeRepository.CreateItemAsync(rewardDistributionType);
-
-            RewardingPlayerType rewardingPlayerType = new RewardingPlayerType()
-            {
-                RewardingPlayerTypeId = 1,
-                RewardingPlayerTypeName = "Lottery"
-            };
-            await rewardingPlayerTypeRepository.CreateItemAsync(rewardingPlayerType);
-
-            rewardingPlayerType = new RewardingPlayerType()
-            {
-                RewardingPlayerTypeId = 2,
-                RewardingPlayerTypeName = "Leaderboard"
-            };
-            await rewardingPlayerTypeRepository.CreateItemAsync(rewardingPlayerType);
-
-            rewardingPlayerType = new RewardingPlayerType()
-            {
-                RewardingPlayerTypeId = 3,
-                RewardingPlayerTypeName = "For all"
-            };
-            await rewardingPlayerTypeRepository.CreateItemAsync(rewardingPlayerType);
-
-            GameSubscriptionType gameSubscriptionType = new GameSubscriptionType()
-            {
-                GameSubscriptionTypeId = 1,
-                GameSubscriptionTypeName = "Daily"
-            };
-
-            await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
-
-            gameSubscriptionType = new GameSubscriptionType()
-            {
-                GameSubscriptionTypeId = 2,
-                GameSubscriptionTypeName = "1 Month"
-            };
-            await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
-
-            gameSubscriptionType = new GameSubscriptionType()
-            {
-                GameSubscriptionTypeId = 3,
-                GameSubscriptionTypeName = "3 Month"
-            };
-            await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
-
-            gameSubscriptionType = new GameSubscriptionType()
-            {
-                GameSubscriptionTypeId = 4,
-                GameSubscriptionTypeName = "1 Year"
-            };
-            await gameSubscriptionTypeRepository.CreateItemAsync(gameSubscriptionType);
-
-            GameMode gameMode = new GameMode()
-            {
-                GameModeId = 1,
-                GameModeName ="Timer countdown"
-            };
-            await gameModeRepository.CreateItemAsync(gameMode);
-
-            gameMode = new GameMode()
-            {
-                GameModeId = 2,
-                GameModeName = "Points countdown"
-            };
-            await gameModeRepository.CreateItemAsync(gameMode);
-
-            gameMode = new GameMode()
-            {
-                GameModeId = 3,
-                GameModeName = "Round based"
-            };
-            await gameModeRepository.CreateItemAsync(gameMode);
+            }
+            
         }
 
     }
